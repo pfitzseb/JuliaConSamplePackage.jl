5d28825c86d7785da8a7970803e003b7403e7e5f
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-07-23 18:44:09 +0200

    actual implemention

--- src/JuliaConSamplePackage.jl
+++ src/JuliaConSamplePackage.jl
@@ -1,5 +1,37 @@
 module JuliaConSamplePackage
+using ForwardDiff
 
-# Write your package code here.
+export newton
+
+"""
+    newton(f, f′, x₀; maxiter = 100, tol = 1e-8)
+
+Find `f`s root via Newton's method.
+"""
+function newton(f, f′, x₀; maxiter = 100, tol = 1e-8)
+    xₙ₊₁ = x₀
+    xₙ = NaN
+    iter = 0
+    while true
+        fₓ₊₁ = f(xₙ₊₁)
+        fₓ = f(xₙ)
+        if abs(fₓ₊₁ - fₓ) < tol
+            return xₙ₊₁
+        end
+        if iter > maxiter
+            error("Did not converge!")
+        end
+
+        f′ₙ₊₁ = f′(xₙ₊₁)
+        if f′ₙ₊₁ == 0.0
+            error("Derivative is zero!")
+        end
+        xₙ₊₁, xₙ = xₙ₊₁ - fₓ₊₁/f′ₙ₊₁, xₙ₊₁
+        iter += 1
+    end
+end
+
+newton(f, x₀; maxiter = 100, tol = 1e-8) =
+    newton(f, x -> ForwardDiff.derivative(f, x), x₀; maxiter = maxiter, tol = tol)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using JuliaConSamplePackage
 using Test
 
 @testset "JuliaConSamplePackage.jl" begin
-    # Write your tests here.
+    @test JuliaConSamplePackage.newton(sin, cos, 1.0) ≈ 0.0
+    @test JuliaConSamplePackage.newton(cos, x -> -sin(x), 1.0) ≈ pi/2
+    @test JuliaConSamplePackage.newton(x -> x^2 - 2x - 1, x -> 2x - 2, 3.0)  ≈ 2.414213562373095
+    @test abs(JuliaConSamplePackage.newton(x -> exp(x) - cos(x), x -> exp(x) + sin(x), 3.0) - 0.0) <= 1e-14
+    @test abs(JuliaConSamplePackage.newton(x -> x^5 - x - 1, x -> 5x^4 - 1, 1.0) - 1.1673039782614187) <= 1e-14
+
+    @testset "autodiff" begin
+        @test abs(JuliaConSamplePackage.newton(sin, 0.5) - 0.0) <= 100*eps(1.0)
+        @test JuliaConSamplePackage.newton(cos, 1.0) ≈ pi/2
+        @test JuliaConSamplePackage.newton(x -> x^2 - 2x - 1, 3.0)  ≈ 2.414213562373095
+        @test abs(JuliaConSamplePackage.newton(x -> exp(x) - cos(x), 3.0) - 0.0) <= 1e-14
+        @test abs(JuliaConSamplePackage.newton(x -> cos(x) - x^3, 3.0) - 0.8654740331016144) <= 1e-14
+    end
 end
